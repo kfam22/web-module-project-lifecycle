098347223d746bb114a543c87f0ccb4ec4bc8e0b
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2022-01-11 16:09:15 -0500

    built out dom structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = {
+    user: [],
+    followers: []
+  }
+  
   render() {
     return(<div>
-      Github Card
+      {/* header */}
+      <h1>Github Info</h1>
+       
+       {/* form */}
+       <form>
+         <input/>
+         <button>Search</button>
+       </form>
+
+       {/* user component */}
+       <div className='userImageContainer'>
+          <img src='#'/>
+       </div>
+       <div className='userInfoContainer'>
+          <h2>User Login</h2>
+          <p>User url</p>
+          <p>Total Repos: 100</p>
+          <p>Total Followers: 43</p>
+       </div>
+
+       {/* follower */}
+       <div className='follower'>
+         <img src='#'/>
+         <h4>follower name</h4>
+       </div>
+
+       {/* follower list */}
+       {/* maps over followers arr from state and returns an image and user login */}
+
     </div>);
   }
 }
